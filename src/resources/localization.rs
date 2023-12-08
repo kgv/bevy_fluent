e6e2ab63563485ecedd933694fc242735aae5e09
Christian Sandven <christian.sandven@amedia.no>
2023-12-08 21:41:35 +0100

    Add support for parameter parsing

--- src/resources/localization.rs
+++ src/resources/localization.rs
@@ -42,14 +42,34 @@ impl Localization {
 impl<'a, T, U> Content<'a, T, U> for Localization
 where
     T: Copy + Into<Request<'a, U>>,
-    U: Borrow<FluentArgs<'a>>,
+    U: Borrow<FluentArgs<'a>> + Debug,
 {
     #[instrument(fields(request = %request.into()), skip_all)]
     fn content(&self, request: T) -> Option<String> {
         self.0.values().find_map(|bundle| {
-            let content = bundle.content(request);
-            trace!(locale = %bundle.locale(), ?content);
-            content
+            let request = request.into();
+            let request = request.borrow();
+            let message = bundle.get_message(request.id)?;
+
+            let pattern = match request.attr {
+                Some(key) if !key.is_empty() => message.get_attribute(key)?.value(),
+                _ => message.value()?,
+            };
+
+            let mut errors = Vec::new();
+            let content = bundle
+                .format_pattern(
+                    pattern,
+                    request.args.as_ref().map(Borrow::borrow),
+                    &mut errors,
+                )
+                .to_string();
+
+            for error in &errors {
+                error!(%error);
+            }
+
+            Some(content.replace("\u{2068}", "").replace("\u{2069}", "")) // Dirty hack to strip any "First Strong Isolate" unicode values
         })
     }
 }
