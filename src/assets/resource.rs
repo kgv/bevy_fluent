ec6ee62d43d93738b35d40e701282562a3e32397
Brandon Reinhart <slaanesh@gmail.com>
2024-03-04 23:08:40 -0600

    remove typeuuid

--- src/assets/resource.rs
+++ src/assets/resource.rs
@@ -4,7 +4,7 @@ use super::{Error, Result};
 use bevy::{
     asset::{io::Reader, AssetLoader, AsyncReadExt, LoadContext},
     prelude::*,
-    reflect::{TypePath, TypeUuid},
+    reflect::TypePath,
     utils::{
         tracing::{self, instrument},
         BoxedFuture,
@@ -14,8 +14,7 @@ use fluent::FluentResource;
 use std::{ops::Deref, str, sync::Arc};
 
 /// [`FluentResource`](fluent::FluentResource) wrapper
-#[derive(Asset, Clone, Debug, TypePath, TypeUuid)]
-#[uuid = "0b2367cb-fb4a-4746-a305-df98b26dddf6"]
+#[derive(Asset, Clone, Debug, TypePath)]
 pub struct ResourceAsset(pub(crate) Arc<FluentResource>);
 
 impl Deref for ResourceAsset {
