b5829bc462f086e0b1acd921acde0e60bd572bcc
Brandon Reinhart <brandon@deadmoney.gg>
2024-07-04 14:56:21 -0500

    update to bevy 0.14

--- src/assets/resource.rs
+++ src/assets/resource.rs
@@ -5,7 +5,7 @@ use bevy::{
     asset::{io::Reader, AssetLoader, AsyncReadExt, LoadContext},
     prelude::*,
     reflect::TypePath,
-    utils::{tracing::instrument, BoxedFuture},
+    utils::tracing::instrument,
 };
 use fluent::FluentResource;
 use std::{ops::Deref, str, sync::Arc};
@@ -32,17 +32,15 @@ impl AssetLoader for ResourceAssetLoader {
     type Settings = ();
     type Error = Error;
 
-    fn load<'a>(
-        &self,
-        reader: &'a mut Reader,
+    async fn load<'a>(
+        &'a self,
+        reader: &'a mut Reader<'_>,
         _: &'a Self::Settings,
-        _: &'a mut LoadContext,
-    ) -> BoxedFuture<'a, Result<Self::Asset>> {
-        Box::pin(async move {
-            let mut content = String::new();
-            reader.read_to_string(&mut content).await?;
-            Ok(ResourceAsset(deserialize(content)))
-        })
+        _: &'a mut LoadContext<'_>,
+    ) -> Result<Self::Asset, Self::Error> {
+        let mut content = String::new();
+        reader.read_to_string(&mut content).await?;
+        Ok(ResourceAsset(deserialize(content)))
     }
 
     fn extensions(&self) -> &[&str] {
