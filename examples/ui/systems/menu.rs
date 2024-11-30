8a231ddbe97bb53624c37a8bf4fc63c3dc079b23
Ethan Webb <ethanmwebb@gmail.com>
2024-11-30 09:47:11 +0000

    update bevy to version 0.15 and make all required migration changes including replacing deprecated bundles

--- examples/ui/systems/menu.rs
+++ examples/ui/systems/menu.rs
@@ -22,69 +22,56 @@ pub fn setup(
         .unwrap()
         .to_sentence_case();
     // camera
-    commands.spawn(Camera2dBundle::default()).insert(Menu);
+    commands.spawn(Camera2d).insert(Menu);
     // ui
     commands
-        .spawn(NodeBundle {
-            style: Style {
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                position_type: PositionType::Absolute,
-                flex_direction: FlexDirection::ColumnReverse,
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::FlexStart,
-                ..default()
-            },
-            background_color: Color::NONE.into(),
+        .spawn((Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            position_type: PositionType::Absolute,
+            flex_direction: FlexDirection::ColumnReverse,
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::FlexStart,
             ..default()
-        })
+        },
+        BackgroundColor(Color::NONE)))
         .insert(Menu)
         .with_children(|parent| {
             // Header
             parent
-                .spawn(NodeBundle {
-                    style: Style {
+                .spawn((Node {
                         width: Val::Percent(100.0),
                         height: Val::Percent(25.0),
                         justify_content: JustifyContent::Center,
                         align_items: AlignItems::Center,
                         ..default()
                     },
-                    background_color: css::DARK_GRAY.into(),
-                    ..default()
-                })
+                    BackgroundColor(css::DARK_GRAY.into()),
+                ))
                 .with_children(|parent| {
-                    parent.spawn(TextBundle {
-                        text: Text::from_section(
-                            &choose_language,
-                            TextStyle {
-                                font: font.0.clone(),
-                                font_size: 64.0,
-                                color: Color::WHITE,
-                            },
-                        ),
+                    parent.spawn((Text(choose_language),
+                    TextFont{
+                        font: font.0.clone(),
+                        font_size: 64.0,
                         ..default()
-                    });
-                });
+                    },
+                    TextColor(Color::WHITE),
+                    ));});
             // Content
             parent
-                .spawn(NodeBundle {
-                    style: Style {
+                .spawn(Node {
                         width: Val::Percent(100.0),
                         height: Val::Percent(75.0),
                         justify_content: JustifyContent::Center,
                         align_items: AlignItems::Center,
                         margin: UiRect::all(Val::Auto),
                         ..default()
-                    },
-                    ..default()
                 })
                 .with_children(|parent| {
                     parent
                         .spawn((
                             PreviousButton,
-                            ButtonBundle {
-                                style: Style {
+                            Node {
                                     width: Val::Percent(10.0),
                                     height: Val::Percent(20.0),
                                     min_width: Val::Px(64.0),
@@ -92,27 +79,22 @@ pub fn setup(
                                     justify_content: JustifyContent::Center,
                                     align_items: AlignItems::Center,
                                     ..default()
-                                },
-                                background_color: css::GRAY.into(),
-                                ..default()
                             },
+                            BackgroundColor(css::GRAY.into()),
+                            Button
                         ))
                         .with_children(|parent| {
-                            parent.spawn(TextBundle {
-                                text: Text::from_section(
-                                    "<",
-                                    TextStyle {
-                                        font: font.0.clone(),
-                                        font_size: 64.0,
-                                        color: Color::WHITE,
-                                    },
-                                ),
+                            parent.spawn((Text("<".to_string()),
+                            TextFont{
+                                font: font.0.clone(),
+                                font_size: 64.0,
                                 ..default()
-                            });
+                            },
+                            TextColor(Color::WHITE),
+                            ));
                         });
                     parent
-                        .spawn(NodeBundle {
-                            style: Style {
+                        .spawn((Node{
                                 width: Val::Percent(80.0),
                                 height: Val::Percent(20.0),
                                 min_width: Val::Px(256.0),
@@ -121,27 +103,22 @@ pub fn setup(
                                 align_items: AlignItems::Center,
                                 ..default()
                             },
-                            background_color: css::GRAY.into(),
-                            ..default()
-                        })
+                            BackgroundColor(css::GRAY.into()),
+                        ))
                         .with_children(|parent| {
-                            parent.spawn(TextBundle {
-                                text: Text::from_section(
-                                    &locale,
-                                    TextStyle {
-                                        font: font.0.clone(),
-                                        font_size: 64.0,
-                                        color: Color::WHITE,
-                                    },
-                                ),
+                            parent.spawn((Text(locale),
+                            TextFont{
+                                font: font.0.clone(),
+                                font_size: 64.0,
                                 ..default()
-                            });
+                            },
+                            TextColor(Color::WHITE),
+                            ));
                         });
                     parent
                         .spawn((
                             NextButton,
-                            ButtonBundle {
-                                style: Style {
+                            Node {
                                     width: Val::Percent(10.0),
                                     height: Val::Percent(20.0),
                                     min_width: Val::Px(64.0),
@@ -150,22 +127,19 @@ pub fn setup(
                                     align_items: AlignItems::Center,
                                     ..default()
                                 },
-                                background_color: css::GRAY.into(),
-                                ..default()
-                            },
+                                BackgroundColor(css::GRAY.into()),
+                                Button
+                            ,
                         ))
                         .with_children(|parent| {
-                            parent.spawn(TextBundle {
-                                text: Text::from_section(
-                                    ">",
-                                    TextStyle {
-                                        font: font.0.clone(),
-                                        font_size: 64.0,
-                                        color: Color::WHITE,
-                                    },
-                                ),
+                            parent.spawn((Text(">".to_string()),
+                            TextFont{
+                                font: font.0.clone(),
+                                font_size: 64.0,
                                 ..default()
-                            });
+                            },
+                            TextColor(Color::WHITE),
+                            ));
                         });
                 });
         });
--- src/assets/bundle.rs
+++ src/assets/bundle.rs
@@ -37,11 +37,11 @@ impl AssetLoader for BundleAssetLoader {
     type Settings = ();
     type Error = Error;
 
-    async fn load<'a>(
-        &'a self,
-        reader: &'a mut Reader<'_>,
-        _: &'a Self::Settings,
-        load_context: &'a mut LoadContext<'_>,
+    async fn load(
+        &self,
+        reader: &mut dyn Reader,
+        _: &Self::Settings,
+        load_context: &mut LoadContext<'_>,
     ) -> Result<Self::Asset, Self::Error> {
         let path = load_context.path();
         let mut content = String::new();
@@ -79,7 +79,7 @@ async fn load(data: Data, load_context: &mut LoadContext<'_>) -> Result<BundleAs
                 path = parent.join(path);
             }
         }
-        let loaded = load_context.loader().direct().untyped().load(path).await?;
+        let loaded = load_context.loader().immediate().with_unknown_type().load(path).await?;
         let resource = loaded.get::<ResourceAsset>().unwrap();
         if let Err(errors) = bundle.add_resource(resource.0.clone()) {
             warn_span!("add_resource").in_scope(|| {
